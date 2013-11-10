6f18fb179995c11392442fedf8b593f8fa6822b7
Rory McCann <rory@technomancy.org>
2013-11-10 13:07:19 +0000

    include templates

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup
 
 setup(name="django-osm-irish-townlands",
-      version="0.0.1",
+      version="0.1.0",
       author="Rory McCann",
       author_email="rory@technomancy.org",
       packages=['irish_townlands', 'irish_townlands.management.commands'],
+      package_data={'irish_townlands': ['templates/irish_townlands/*']},
       license='GPLv3',
       description='',
       install_requires=[
