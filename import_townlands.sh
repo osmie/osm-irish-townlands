342affe34064059495070c636b41c24b1ea576d1
Rory McCann <rory@technomancy.org>
2014-03-11 21:39:05 +0000

    delete tables to save disk space

--- import_townlands.sh
+++ import_townlands.sh
@@ -4,16 +4,20 @@ set -o errexit
 
 cd $(dirname $0)
 
-PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists valid_polygon;"
-PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists water_polygon;"
+# In case these are still around
+for TABLE in valid_polygon water_polygon planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
+	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
+done
+
 
 wget -q -N http://download.geofabrik.de/openstreetmap/europe/ireland-and-northern-ireland.osm.pbf
 osm2pgsql --cache 50M --slim --style ./osm2pgsql.style -G ireland-and-northern-ireland.osm.pbf
 rm ireland-and-northern-ireland.osm.pbf
 
 # not needed anymore
-psql -q -d gis -c "drop table if exists planet_osm_nodes ; drop table if exists planet_osm_rels ; drop table if exists planet_osm_ways ;"
-psql -q -d gis -c "drop table planet_osm_line; drop table planet_osm_point; drop table planet_osm_roads;"
+for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads; do
+	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
+done
 
 psql -q -d gis -c "create table valid_polygon as select * from planet_osm_polygon where name IS NOT NULL and st_isvalid(way) and (admin_level is not null or boundary is not null)";
 psql -q -d gis -c "create table water_polygon as select way from planet_osm_polygon where water IS NOT NULL OR waterway IS NOT NULL OR \"natural\" = 'water' and st_isvalid(way)";
