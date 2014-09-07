599280b7f15e7d2fc6e2120bf228c0f3acc7c7c8
Rory McCann <rory@technomancy.org>
2014-09-07 15:21:40 +0100

    make work if not there

--- import_townlands.sh
+++ import_townlands.sh
@@ -6,8 +6,8 @@ BASEDIR=$(dirname $0)
 cd ${BASEDIR}
 
 # In case these are still around
-psql -q -d gis -c "truncate table valid_polygon;"
-psql -q -d gis -c "truncate table water_polygon;"
+psql -q -d gis -c "truncate table if exists valid_polygon;"
+psql -q -d gis -c "truncate table if exists water_polygon;"
 for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
 	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
 done
@@ -22,13 +22,21 @@ for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line pl
 	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
 done
 
+psql -s gis "create table if not exists valid_polygon like plant_osm_polygon;"
 psql -q -d gis -c "insert into valid_polygon as select * from planet_osm_polygon where name IS NOT NULL and st_isvalid(way) and (admin_level is not null or boundary is not null)";
+
+psql -s gis "create table if not exists water_polygon like plant_osm_polygon;"
 psql -q -d gis -c "insert into water_polygon as select way from planet_osm_polygon where water IS NOT NULL OR waterway IS NOT NULL OR \"natural\" = 'water' and st_isvalid(way)";
 
 psql -q -d gis -c "drop table planet_osm_polygon;"
 
+psql -q -d gis -c "create index valid_polygon__way on valid_polygon using GIST (way); create index valid_polygon__admin_level on valid_polygon (admin_level); create index valid_polygon__name on valid_polygon (name);"
+psql -q -d gis -c "create index water_polygon__way on water_polygon using GIST (way);"
+
+psql -q -d gis -c "alter table valid_polygon add column geo geography;"
 psql -q -d gis -c "update valid_polygon set geo = st_geographyfromtext(st_astext(st_transform(way, 4326)));"
 
+psql -q -d gis -c "alter table water_polygon add column geo geography;"
 psql -q -d gis -c "update valid_polygon set geo = st_geographyfromtext(st_astext(st_transform(way, 4326)));"
 
 cd ${BASEDIR}
