0796ff15a0370745de06b940e5da6524ebefcff8
Rory McCann <rory@technomancy.org>
2014-09-07 19:17:01 +0100

    Revert "temp disable"
    This reverts commit 3aff23871af322fa6c405341cdf26534207bd5e1.

--- import_townlands.sh
+++ import_townlands.sh
@@ -6,16 +6,16 @@ BASEDIR=$(dirname $0)
 cd ${BASEDIR}
 
 # In case these are still around
-#psql -q -d gis -c "truncate table valid_polygon;" || true
-#psql -q -d gis -c "truncate table water_polygon;" || true
-#for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
-#	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
-#done
+psql -q -d gis -c "truncate table valid_polygon;" || true
+psql -q -d gis -c "truncate table water_polygon;" || true
+for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
+	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
+done
 
 
-#wget -q -N http://download.geofabrik.de/europe/ireland-and-northern-ireland-latest.osm.pbf
-#osm2pgsql --cache 200M --cache-strategy sparse --slim --style ./osm2pgsql.style -G ireland-and-northern-ireland-latest.osm.pbf
-#rm ireland-and-northern-ireland-latest.osm.pbf
+wget -q -N http://download.geofabrik.de/europe/ireland-and-northern-ireland-latest.osm.pbf
+osm2pgsql --cache 200M --cache-strategy sparse --slim --style ./osm2pgsql.style -G ireland-and-northern-ireland-latest.osm.pbf
+rm ireland-and-northern-ireland-latest.osm.pbf
 
 # not needed anymore
 for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads; do
