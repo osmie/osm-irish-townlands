2fc6e9f9dd2d065698b134987c42747666833548
Rory McCann <rory@technomancy.org>
2014-09-07 14:57:04 +0100

    don't delete valid_polygon, otherwise permissions get lost

--- import_townlands.sh
+++ import_townlands.sh
@@ -6,7 +6,9 @@ BASEDIR=$(dirname $0)
 cd ${BASEDIR}
 
 # In case these are still around
-for TABLE in valid_polygon water_polygon planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
+psql -q -d gis -c "truncate table valid_polygon;"
+psql -q -d gis -c "truncate table water_polygon;"
+for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
 	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
 done
 
@@ -20,19 +22,13 @@ for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line pl
 	PGOPTIONS="--client-min-messages=warning" psql -q -d gis -c "drop table if exists $TABLE;"
 done
 
-psql -q -d gis -c "create table valid_polygon as select * from planet_osm_polygon where name IS NOT NULL and st_isvalid(way) and (admin_level is not null or boundary is not null)";
-psql -q -d gis -c "create table water_polygon as select way from planet_osm_polygon where water IS NOT NULL OR waterway IS NOT NULL OR \"natural\" = 'water' and st_isvalid(way)";
+psql -q -d gis -c "insert into valid_polygon as select * from planet_osm_polygon where name IS NOT NULL and st_isvalid(way) and (admin_level is not null or boundary is not null)";
+psql -q -d gis -c "insert into water_polygon as select way from planet_osm_polygon where water IS NOT NULL OR waterway IS NOT NULL OR \"natural\" = 'water' and st_isvalid(way)";
 
 psql -q -d gis -c "drop table planet_osm_polygon;"
 
-
-psql -q -d gis -c "create index valid_polygon__way on valid_polygon using GIST (way); create index valid_polygon__admin_level on valid_polygon (admin_level); create index valid_polygon__name on valid_polygon (name);"
-psql -q -d gis -c "create index water_polygon__way on water_polygon using GIST (way);"
-
-psql -q -d gis -c "alter table valid_polygon add column geo geography;"
 psql -q -d gis -c "update valid_polygon set geo = st_geographyfromtext(st_astext(st_transform(way, 4326)));"
 
-psql -q -d gis -c "alter table water_polygon add column geo geography;"
 psql -q -d gis -c "update valid_polygon set geo = st_geographyfromtext(st_astext(st_transform(way, 4326)));"
 
 cd ${BASEDIR}
