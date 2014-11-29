1b6f9ee6a8783f1768a9e5d10c92cd287fdb701c
Rory McCann <rory@technomancy.org>
2014-11-29 18:16:15 +0100

    Drop tables when starting off

--- import_townlands.sh
+++ import_townlands.sh
@@ -12,8 +12,8 @@ EXPORTED_FILES_DIR=$3
 POSTGIS_CMD="psql -q -U ${DB_USER} -h localhost -d gis"
 
 # In case these are still around
-PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "truncate table valid_polygon;" 2>/dev/null || true
-PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "truncate table water_polygon;" 2>/dev/null || true
+PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "drop table valid_polygon;" 2>/dev/null || true
+PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "drop table water_polygon;" 2>/dev/null || true
 for TABLE in planet_osm_nodes planet_osm_rels planet_osm_ways planet_osm_line planet_osm_line planet_osm_point planet_osm_roads planet_osm_polygon planet_osm_roads_tmp; do
 	PGOPTIONS="--client-min-messages=warning" PGPASSWORD=$DB_PASS $POSTGIS_CMD -c "drop table if exists $TABLE;"
 done
