f968a6e97fd0bd7a15a57dacf66a17802419fb19
Rory McCann <rory@technomancy.org>
2015-03-23 22:26:27 +0100

    move exporting out to other update script

--- import_townlands.sh
+++ import_townlands.sh
@@ -59,45 +59,6 @@ PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "update valid_polygon set geo = st_transfo
 PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "alter table water_polygon add column geo geography;" 2>/dev/null || true
 PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "update water_polygon set geo = st_transform(way, 4326)::geography;"
 
-function dump() {
-    PREFIX=$1
-    WHERE=$2
-    rm -f ${PREFIX}
-    pgsql2shp -f ${PREFIX} -u "${DB_USER}" -h localhost -P "${DB_PASS}" townlands "select osm_id, name, \"name:ga\", \"name:en\", alt_name, \"alt_name:ga\", st_area(geo) as area_m2, ST_X(st_transform((ST_centroid(way)), 4326)) as latitude, ST_Y(st_transform((ST_centroid(way)), 4326)) as longitude, geo from valid_polygon where ${WHERE}" >/dev/null
-}
-
-
-# dump townlands etc as shapefiles
-mkdir -p $EXPORTED_FILES_DIR
-dump ${EXPORTED_FILES_DIR}/provinces "admin_level = '5'"
-dump ${EXPORTED_FILES_DIR}/counties "admin_level = '6'"
-dump ${EXPORTED_FILES_DIR}/townlands "admin_level = '10'"
-dump ${EXPORTED_FILES_DIR}/baronies "boundary = 'barony'"
-dump ${EXPORTED_FILES_DIR}/civil_parishes "boundary = 'civil_parish'"
-dump ${EXPORTED_FILES_DIR}/eds "admin_level = '9'"
-
-pushd ${EXPORTED_FILES_DIR} > /dev/null
-for TYPE in townlands counties baronies civil_parishes provinces eds ; do
-
-    rm -f ${TYPE}.geojson
-    ogr2ogr -f "GeoJSON" ${TYPE}.geojson ${TYPE}.shp
-    zip -q ${TYPE}.geojson.zip ${TYPE}.geojson
-    rm -f ${TYPE}.geojson
-
-    rm -f doc.kml
-    ogr2ogr -f "KML" doc.kml ${TYPE}.shp -dsco NameField=name
-    zip -q ${TYPE}.kmz doc.kml
-    rm -f doc.kml
-
-    rm -f ${TYPE}.csv ${TYPE}.csv.zip
-    ogr2ogr -f CSV ${TYPE}.csv ${TYPE}.shp -lco GEOMETRY=AS_WKT
-    zip -q ${TYPE}.csv.zip ${TYPE}.csv
-    rm -f ${TYPE}.csv
-
-    zip -q ${TYPE}.zip ${TYPE}.dbf ${TYPE}.prj ${TYPE}.shp ${TYPE}.shx
-    rm -f ${TYPE}.dbf ${TYPE}.prj ${TYPE}.shp ${TYPE}.shx
-done
-popd > /dev/null
 
 
 cd ${BASEDIR}
