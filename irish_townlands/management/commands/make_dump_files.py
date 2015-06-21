504a41f61259855f28540ab5007347f94a712b74
Rory McCann <rory@technomancy.org>
2015-06-21 15:42:27 +0200

    attempt to work

--- irish_townlands/management/commands/make_dump_files.py
+++ irish_townlands/management/commands/make_dump_files.py
@@ -53,10 +53,11 @@ class Command(BaseCommand):
             FROM (
                 SELECT
                     t.osm_id, t.name, t.name_ga, t.name_en, t.alt_name, t.alt_name_ga,
-                    t.osm_user, t.osm_timestamp, c.name AS co_name, c.osm_id AS co_osm_id,
-                    cp.name AS cp_name, cp.osm_id AS cp_osm_id, ed.name AS ed_name,
+                    t.osm_user, t.osm_timestamp, t.attribution,
+                    c.name AS co_name, c.osm_id AS co_osm_id, cp.name AS cp_name,
+                    cp.osm_id AS cp_osm_id, ed.name AS ed_name,
                     ed.osm_id AS ed_osm_id, b.name AS bar_name, b.osm_id AS bar_osm_id,
-                    CONCAT('http://www.townlands.ie/', t.url_path) AS t_ie_url,
+                    concat('http://www.townlands.ie/', t.url_path) AS t_ie_url,
                     ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
                 FROM
                     irish_townlands_townland AS t
@@ -69,8 +70,9 @@ class Command(BaseCommand):
                         ON (cp.id = t.civil_parish_id)
                     LEFT OUTER JOIN irish_townlands_electoraldivision AS ed
                         ON (ed.id = t.ed_id)
-                ) AS xx
+                ) AS xx;
             """)
+        return
         dump(directory, "counties", dbuser, dbpass, """
             SELECT
                 xx.*, ST_area(xx.geom::geography) AS area,
@@ -79,8 +81,8 @@ class Command(BaseCommand):
             FROM (
                 SELECT
                     c.osm_id, c.name, c.name_ga, c.name_en, c.alt_name, c.alt_name_ga,
-                    c.osm_user, c.osm_timestamp,
-                    CONCAT('http://www.townlands.ie/', c.url_path) AS t_ie_url,
+                    c.osm_user, c.osm_timestamp, c.attribution,
+                    concat('http://www.townlands.ie/', c.url_path) AS t_ie_url,
                     ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
                 FROM
                     irish_townlands_county AS c
@@ -96,9 +98,9 @@ class Command(BaseCommand):
             FROM (
                 SELECT
                     b.osm_id, b.name, b.name_ga, b.name_en, b.alt_name,
-                    b.alt_name_ga, b.osm_user, b.osm_timestamp, c.name AS co_name,
-                    c.osm_id AS co_osm_id,
-                    CONCAT('http://www.townlands.ie/', b.url_path) AS t_ie_url,
+                    b.alt_name_ga, b.osm_user, b.osm_timestamp, b.attribution,
+                    c.name AS co_name, c.osm_id AS co_osm_id,
+                    concat('http://www.townlands.ie/', b.url_path) AS t_ie_url,
                     ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
                 FROM
                     irish_townlands_barony AS b
@@ -115,7 +117,8 @@ class Command(BaseCommand):
             FROM (
                 SELECT
                     cp.osm_id, cp.name, cp.name_ga, cp.name_en, cp.alt_name, cp.alt_name_ga,
-                    cp.osm_user, cp.osm_timestamp, c.name as co_name, c.osm_id as co_osm_id,
+                    cp.osm_user, cp.osm_timestamp, cp.attribution,
+                    c.name as co_name, c.osm_id as co_osm_id,
                     concat('http://www.townlands.ie/', cp.url_path) AS t_ie_url,
                     ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
                 FROM
@@ -133,8 +136,9 @@ class Command(BaseCommand):
             FROM (
                 SELECT
                     ed.osm_id, ed.name, ed.name_ga, ed.name_en, ed.alt_name, ed.alt_name_ga,
-                    ed.osm_user, ed.osm_timestamp, c.name as co_name, c.osm_id AS co_osm_id,
-                    CONCAT('http://www.townlands.ie/', ed.url_path) AS t_ie_url,
+                    ed.osm_user, ed.osm_timestamp, ed.attribution,
+                    c.name as co_name, c.osm_id AS co_osm_id,
+                    concat('http://www.townlands.ie/', ed.url_path) AS t_ie_url,
                     ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
                 FROM
                     irish_townlands_electoraldivision AS ed
