24a9ff136956233648380e9ef4185a6e1f5c54df
Rory McCann <rory@technomancy.org>
2015-10-17 11:47:44 +0200

    Ensure the shapefile knows the SRS/SRID of the exported objs
    fixes #100

--- irish_townlands/management/commands/make_dump_files.py
+++ irish_townlands/management/commands/make_dump_files.py
@@ -78,7 +78,7 @@ class Command(BaseCommand):
                     cp.osm_id AS cp_osm_id, ed.name AS ed_name,
                     ed.osm_id AS ed_osm_id, b.name AS bar_name, b.osm_id AS bar_osm_id,
                     concat('http://www.townlands.ie/', t.url_path) AS t_ie_url,
-                    ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
+                    ST_SetSRID(ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson), 4326) AS geom
                 FROM
                     irish_townlands_townland AS t
                         JOIN irish_townlands_polygon ON (t._polygon_geojson_id = irish_townlands_polygon.id)
@@ -101,7 +101,7 @@ class Command(BaseCommand):
                     c.osm_id, c.name, c.name_ga, c.name_en, c.alt_name, c.alt_name_ga,
                     c.osm_user, c.osm_timestamp, c.attribution,
                     concat('http://www.townlands.ie/', c.url_path) AS t_ie_url,
-                    ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
+                    ST_SetSRID(ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson), 4326) AS geom
                 FROM
                     irish_townlands_county AS c
                         JOIN irish_townlands_polygon
@@ -120,7 +120,7 @@ class Command(BaseCommand):
                     b.alt_name_ga, b.osm_user, b.osm_timestamp, b.attribution,
                     c.name AS co_name, c.osm_id AS co_osm_id,
                     concat('http://www.townlands.ie/', b.url_path) AS t_ie_url,
-                    ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
+                    ST_SetSRID(ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson), 4326) AS geom
                 FROM
                     irish_townlands_barony AS b
                         JOIN irish_townlands_polygon ON (b._polygon_geojson_id = irish_townlands_polygon.id)
@@ -141,7 +141,7 @@ class Command(BaseCommand):
                     cp.osm_user, cp.osm_timestamp, cp.attribution,
                     c.c_names as co_names, c.c_osm_ids as co_osm_ids,
                     concat('http://www.townlands.ie/', cp.url_path) AS t_ie_url,
-                    ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
+                    ST_SetSRID(ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson), 4326) AS geom
                 FROM
                     irish_townlands_civilparish AS cp
                         JOIN irish_townlands_polygon ON (cp._polygon_geojson_id = irish_townlands_polygon.id)
@@ -174,7 +174,7 @@ class Command(BaseCommand):
                     ed.osm_user, ed.osm_timestamp, ed.attribution,
                     c.name as co_name, c.osm_id AS co_osm_id,
                     concat('http://www.townlands.ie/', ed.url_path) AS t_ie_url,
-                    ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson) AS geom
+                    ST_SetSRID(ST_GeomFromGeoJSON(irish_townlands_polygon.polygon_geojson), 4326) AS geom
                 FROM
                     irish_townlands_electoraldivision AS ed
                         JOIN irish_townlands_polygon ON (ed._polygon_geojson_id = irish_townlands_polygon.id)
