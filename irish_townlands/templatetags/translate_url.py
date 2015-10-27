aaaff8176522d38721f2f004d79d0761eafc4d1b
Rory McCann <rory@technomancy.org>
2015-10-27 22:20:18 +0100

    use working snippet

--- irish_townlands/templatetags/translate_url.py
+++ irish_townlands/templatetags/translate_url.py
@@ -1,26 +1,26 @@
 # This Python file uses the following encoding: utf-8
 
-from django import template
-from django.core.urlresolvers import reverse
-from django.core.urlresolvers import resolve
-from django.utils import translation
+from django.template import Library
+from django.core.urlresolvers import resolve, reverse
+from django.utils.translation import activate, get_language
 
-register = template.Library()
+@register.simple_tag(takes_context=True)
+def change_lang(context, lang=None, *args, **kwargs):
+    """
+    Get active page's url by a specified language
+    Usage: {% change_lang 'en' %}
+    """
 
-class TranslatedURL(template.Node):
-    def __init__(self, language):
-        self.language = language
-    def render(self, context):
-        import pudb ; pudb.set_trace()
-        request_language = translation.get_language()
-        translation.activate(request_language)
-        view = resolve(context['request'].path)
-        translation.activate(self.language)
-        url = reverse(view.url_name, args=view.args, kwargs=view.kwargs)
-        translation.activate(request_language)
-        return url
+    path = context['request'].path
+    url_parts = resolve( path )
 
-@register.tag(name='translate_url')
-def do_translate_url(parser, token):
-    language = token.split_contents()[1]
-    return TranslatedURL(language)
+    url = path
+    cur_language = get_language()
+    try:
+        activate(lang)
+        url = reverse( url_parts.view_name, kwargs=url_parts.kwargs )
+    finally:
+        activate(cur_language)
+
+
+    return "%s" % url
