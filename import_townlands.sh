99168c1b1d494f24cb808d95ff60e8dda290fbad
Rory McCann <rory@technomancy.org>
2016-01-05 17:36:31 +0100

    create land not in polygons properly now

--- import_townlands.sh
+++ import_townlands.sh
@@ -82,23 +82,23 @@ PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "analyze water_polygon;" 2> /dev/null || t
 PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "cluster water_polygon using water_polygon__way;" 2> /dev/null || true
 
 # Calculate the coastline
-rm -f coastline.db
 # Long line in case osmcoastline segfaults (has happened to me), I don't want
 # to remove the old land_polygons shapefiles if that command doesn't succeed,
 # so abuse bash this way
-osmcoastline -s 3857 -o coastline.db ireland-and-northern-ireland.osm.pbf && ogr2ogr -f PostgreSQL PG:"dbname=townlands" coastline.db land_polygons -overwrite -nlt MULTIPOLYGON && ogr2ogr -f "ESRI Shapefile" land_polygons.shp coastline.db land_polygons && split-large-polygons  -d townlands -t land_polygons -c wkb_geometry -i ogc_fid -a 10000000 -s 3857 && rm -f land_polygons.dbf land_polygons.prj land_polygons.shp land_polygons.shx && pgsql2shp townlands land_polygons
+rm -f coastline.db && osmcoastline -s 4326 -o coastline.db ireland-and-northern-ireland.osm.pbf  && rm -f land_polygons.dbf land_polygons.prj land_polygons.shp land_polygons.shx && ogr2ogr -f PostgreSQL PG:"dbname=townlands" coastline.db land_polygons -overwrite -nlt MULTIPOLYGON && ogr2ogr -f "ESRI Shapefile" land_polygons.shp coastline.db land_polygons && split-large-polygons  -d townlands -t land_polygons -c wkb_geometry -i ogc_fid -a 0.001 -s 4326 && rm -f land_polygons.dbf land_polygons.prj land_polygons.shp land_polygons.shx && pgsql2shp townlands land_polygons
 rm -f coastline.db
 
 # Make the split table
-PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "create table valid_polygon_split (gid serial primary key, admin_level text, osm_id bigint, geom geometry(MultiPolygon, 900913));" 2> /dev/null || true
+PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "drop table valid_polygon_split; " 2>/dev/null || true
+PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "create table valid_polygon_split (gid serial primary key, admin_level text, osm_id bigint, geom geometry(MultiPolygon, 4326));" 2> /dev/null || true
 PGPASSWORD=${DB_PASS} $POSTGIS_CMD -c "insert into valid_polygon_split (admin_level, osm_id, geom) select admin_level, osm_id, St_multi(way) from valid_polygon;" 2> /dev/null || true
-split-large-polygons  -d townlands -t valid_polygon_split -c geom -i gid -a 10000000 -s 900913
+split-large-polygons  -d townlands -t valid_polygon_split -c geom -i gid -a 0.001 -s 4326
 
 pgsql2shp -f townlands_split.shp townlands "select * from valid_polygon_split where admin_level = '10'" 2>/dev/null
 pgsql2shp -f counties_split.shp townlands "select * from valid_polygon_split where admin_level = '6'" 2>/dev/null
 
 # Land not covered by counties
-difference-polygons -l land_polygons.shp  -r ~/muse/townlands/data/counties_split.shp -a 1e-09 -o not_counties.shp
+difference-polygons -l land_polygons.shp  -r counties_split.shp -a 1e-09 -o not_counties.shp
 
 cd ${BASEDIR}
 #./screenshot-townlands.sh
