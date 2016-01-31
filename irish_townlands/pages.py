877d013dcf37c50d88b3781a8e4aee4691ff3539
Rory McCann <rory@technomancy.org>
2016-01-31 12:27:12 +0100

    more maps

--- irish_townlands/pages.py
+++ irish_townlands/pages.py
@@ -257,6 +257,18 @@ PAGES = {
         <p>This map shows land that is not in any county. It is available in JOSM preset images as "OSMIE T.ie Land not in Counties". If you would like to use it in your own application, the tile URL is <pre>http://www.townlands.ie/tiles/not_counties/{z}/{x}/{y}.png</pre></p>
         <div id="not_counties" data-mapurl="http://www.townlands.ie/tiles/not_counties/{z}/{x}/{y}.png" class="map" style="width:100%; height:700px;"></div>
 
+        <h2>Land not in any Barony</h2>
+        <div id="not_baronies" data-mapurl="http://www.townlands.ie/tiles/not_baronies/{z}/{x}/{y}.png" class="map" style="width:100%; height:700px;"></div>
+
+        <h2>Land not in any Civil Parishes</h2>
+        <div id="not_civil_parishes" data-mapurl="http://www.townlands.ie/tiles/not_civil_parishes/{z}/{x}/{y}.png" class="map" style="width:100%; height:700px;"></div>
+
+        <h2>Land not in any EDs</h2>
+        <div id="not_eds" data-mapurl="http://www.townlands.ie/tiles/not_eds/{z}/{x}/{y}.png" class="map" style="width:100%; height:700px;"></div>
+
+        <h2>Land not in any Townlands</h2>
+        <div id="not_townlands" data-mapurl="http://www.townlands.ie/tiles/not_townlands/{z}/{x}/{y}.png" class="map" style="width:100%; height:700px;"></div>
+
         <script>
             $(document).ready(function() {
                 $(".map").each(function() {
