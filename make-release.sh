dd546dbb8b3d1abaddf4e3a44325fb50e2a45e2b
Rory McCann <rory@technomancy.org>
2016-03-20 19:59:35 +0100

    chore: make release script

---
+++ make-release.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -o errexit
+
+cd $(dirname $0)
+ROOT=$(pwd)
+
+CURR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ $CURR_BRANCH != 'master' ]] ; then
+    echo "[django-osm-irish-townlands] Not on master git branch. Exiting"
+    exit 1
+fi
+
+if ! git diff --exit-code >/dev/null ; then
+    echo "[django-osm-irish-townlands] Uncommited changes. Exiting"
+    git diff --exit-code
+    exit 2
+fi
+
+VERSION=$(date '+%Y%m%d%H%M%S')
+clog -F --setversion=${VERSION} -C CHANGELOG.md
+
+git tag ${VERSION}
